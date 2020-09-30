4f2b23e4f71679f98e9933327483dbabd7835e3d
TroAlexis <aleksei.troshin1997@gmail.com>
2020-09-30 13:36:43 +0300

    Deleted dist folder

--- dist/assets/js/common.2642c1df921de1b7a42c.js
+++
@@ -1,6 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[
-/*!*************************!*\
-  !*** ./src/js/index.js ***!
-  \*************************/
-/*! no static exports found */
-/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(n,w){}]]);
