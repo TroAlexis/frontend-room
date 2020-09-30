57cba6386890fb703de2fe680ced4dd53b68b896
TroAlexis <aleksei.troshin1997@gmail.com>
2020-09-30 23:54:32 +0300

    Updates

---
+++ assets/js/common.ee40c7e3c9a38d017a16.js
@@ -0,0 +1,6 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[
+/*!*******************************************!*\
+  !*** ./src/pug/includes/header/header.js ***!
+  \*******************************************/
+/*! no static exports found */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */,function(e,r){var o=document.querySelector(".header"),n=o.querySelector(".header__burger"),t=o.querySelector(".header__brand"),c=o.querySelector(".header__nav"),l=o.querySelectorAll(".header__link");n.addEventListener("click",(function(){t.classList.toggle("hidden"),n.classList.toggle("open"),c.classList.toggle("open"),l.forEach((function(e){e.classList.toggle("visible")}))}))}]]);
